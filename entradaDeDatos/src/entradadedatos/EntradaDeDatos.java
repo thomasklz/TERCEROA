ef4e08123d800b035792c24fb6deab6dbb6a72d4
thomasklz <pipos_rgt@hotmail.com>
2023-04-26 12:48:22 -0500

    ejerccios de clase

---
+++ entradaDeDatos/src/entradadedatos/EntradaDeDatos.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entradadedatos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EntradaDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+      /* Scanner sc = new Scanner(System.in); 
+       int numero;
+       System.out.println("Ingresa un número");
+       numero=sc.nextInt();
+       System.out.println("El numero es:" + numero);*/
+      
+      
+     /* Scanner sc = new Scanner(System.in);
+      float numero;
+      System.out.println("Ingrese un numero decimal");
+      numero=sc.nextFloat();
+      System.out.println("El numero es:" + numero);*/
+     
+    /* Scanner sc = new Scanner(System.in);
+     String nombre;
+     System.out.println("Ingresa tu nombre");
+     nombre=sc.nextLine();
+     System.out.println("Su nombre es: "+ nombre);*/
+    
+    
+    /*Scanner sc = new Scanner(System.in);
+    boolean estado;
+    System.out.println("Ingresa un estado");
+    estado= sc.nextBoolean();
+    System.out.println("El estado es: "+ estado);*/
+    
+    
+    Scanner sc = new Scanner(System.in);
+    char letra;
+    System.out.println("Ingrese una letra");
+    letra=sc.nextLine().charAt(0);
+    System.out.println("La letra ingresada es: "+ letra);
+     
+     
+    }
+    
+}
