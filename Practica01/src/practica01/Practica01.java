8cb8ddfe5ac5795b098b322ab4392e80b1638e8d
thomasklz <pipos_rgt@hotmail.com>
2023-10-18 09:44:46 -0500

    scanner

---
+++ Practica01/src/practica01/Practica01.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica01;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Practica01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+       
+        System.out.println("Ingrese un numero");
+        numero= sc.nextInt();
+        
+        
+        System.out.println("El numero es: "+numero);
+       
+        
+    }
+    
+}
